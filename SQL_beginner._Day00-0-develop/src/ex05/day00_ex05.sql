e469442c67400298fc4c1342b7948ea9bad7e186
Vladimir Yarushkevich <vv107@ya.ru>
2023-02-26 23:25:51 +0300

    SQL_beginner._Day00
    SQL_beginner._Day00

---
+++ SQL_beginner._Day00-0-develop/src/ex05/day00_ex05.sql
@@ -0,0 +1,16 @@
+-- SELECT * FROM pizzeria
+-- SELECT * FROM person
+-- SELECT * FROM menu
+-- SELECT * FROM person_visits
+-- SELECT * FROM person_order
+
+SELECT
+  ( SELECT name
+   FROM person
+   WHERE person.id = person_order.person_id ) AS NAME
+FROM person_order
+WHERE ( menu_id = 13
+       OR menu_id = 14
+       OR menu_id = 18 )
+  AND order_date = '2022.01.07';
+
---
+++ SQL_beginner._Day00-0-develop/src/ex07/day00_ex07.sql
@@ -0,0 +1,16 @@
+-- SELECT * FROM pizzeria LIMIT 3
+-- SELECT * FROM person LIMIT 3
+-- SELECT * FROM menu LIMIT 3
+-- SELECT * FROM person_visits LIMIT 3
+-- SELECT * FROM person_order LIMIT 3
+
+SELECT id,
+       name,
+       CASE
+           WHEN age BETWEEN 10 AND 20 THEN 'interval #1'
+           WHEN age > 20
+                AND age < 24 THEN 'interval #2'
+           ELSE 'interval #3'
+       END AS interval_info
+FROM person
+ORDER BY interval_info;
